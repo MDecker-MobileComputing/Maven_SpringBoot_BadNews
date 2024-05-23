eac48b9ac9d93b0771369157a3578aa97dd85c16
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-23 14:21:52 +0200

    Projekt erzeugt mit "Spring Initializr", Tab2Space fuer pom.xml gemacht

---
+++ src/main/java/de/eldecker/dhbw/spring/badnews/BadNewsApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.badnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/badnews/BadNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.badnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
